be092903d99e2e5c9bc7da6bc6f8af99b2e1f6e4
WILIAM <wiliamputnomorales45@gmail.com>
2024-11-27 21:13:06 -0500

    cambios nav

--- fronted/src/app/app.routes.ts
+++ fronted/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/general/home/home.component';
+import { LoginComponent } from './pages/general/login/login.component';
+import { SignupComponent } from './pages/general/signup/signup.component';
+import { NotFoundComponent } from './pages/general/not-found/not-found.component';
+
+import { AboutComponent } from './pages/general/about/about.component';
+
+
+import { ContactComponent } from './pages/general/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+
+  {
+    path: 'about', component: AboutComponent,
+  },
+  {
+    path: 'contact', component: ContactComponent,
+
+  },
+
+  { path: '**', component: NotFoundComponent }
+];
